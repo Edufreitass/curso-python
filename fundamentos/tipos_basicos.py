98255980f14dcde4b1ff7be5a6b1fe4d19ec9902
Eduardo Freitas <eduardoflorencio96@gmail.com>
2022-04-19 08:41:05 -0300

    feat: tipos basicos

---
+++ fundamentos/tipos_basicos.py
@@ -0,0 +1,12 @@
+print(True)
+print(False)
+print(1.2 + 1)
+print('Aqui eu falo na minha lingua!')
+print("Tb funciona.")
+print('Você é ' + 3 * 'muito ' + 'legal!')
+# print(3 + '3') -> ambiguidade
+lista = [1, 2, 3]
+print(lista)
+dicionario = {'nome': 'Pedro', 'idade': 22}
+print(dicionario)
+print(None)
