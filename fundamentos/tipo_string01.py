2594e32c909a9ea15f0d2b0501492b8ad192c791
Eduardo Freitas <eduardoflorencio96@gmail.com>
2022-04-25 20:46:02 -0300

    feat: tipo string #01

---
+++ fundamentos/tipo_string01.py
@@ -0,0 +1,22 @@
+print(dir(str))
+nome = 'Saulo Pedro'
+print(nome[0])
+
+print("Dias D'Avila")
+print('Dias D\'Avila')
+
+print("Teste \" funciona!")
+texto = 'Texto entre apostrófos pode ter "aspas"'
+print(texto)
+
+doc = """
+Texto com múltiplas
+    ... linhas
+"""
+print(doc)
+print('Texto com múltiplas\n\t... linhas')
+
+doc2 = '''
+Também é possível\n... com 3 aspas simples
+'''
+print(doc2)
