62f8391c2e6d8f6560d3b29bcd4753f422331182
Eduardo Freitas <eduardoflorencio96@gmail.com>
2022-04-25 21:04:01 -0300

    feat: tipo string #03

---
+++ fundamentos/tipo_string03.py
@@ -0,0 +1,17 @@
+frase = 'Python é uma linguagem excelente'
+print('py' not in frase)
+print('ing' in frase)
+print(len(frase))
+print(frase.lower())
+print(frase)
+print(frase.upper())
+print(frase)
+frase = frase.upper()
+print(frase)
+
+print(frase.split())
+print(frase.split('A'))
+print(frase.split('E'))
+
+print(dir(str))
+help(str.center)
