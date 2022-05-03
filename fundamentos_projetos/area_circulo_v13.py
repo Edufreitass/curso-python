08b11dac7f3070c7604f8458ab640c828bc46ba6
Eduardo Freitas <eduardoflorencio96@gmail.com>
2022-05-02 22:42:31 -0300

    feat: saindo com erro

---
+++ fundamentos_projetos/area_circulo_v13.py
@@ -0,0 +1,21 @@
+import sys
+from math import pi
+
+
+def help():
+    print("É necessário informar o raio do círculo.")
+    print("Sintaxe: {} <raio>".format(sys.argv[0]))
+
+
+def circulo(raio):
+    return pi * float(raio) ** 2
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        help()
+        # sys.exit(1)
+    else:
+        raio = sys.argv[1]
+        area = circulo(raio)
+        print('Área do círculo', area)
