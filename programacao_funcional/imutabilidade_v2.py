7bbb8229d42e7d08e47c504bae29400a82ad35ab
Eduardo Freitas <eduardoflorencio96@gmail.com>
2022-06-13 20:06:37 -0300

    feat: desafio imutabilidade #02

---
+++ programacao_funcional/imutabilidade_v2.py
@@ -0,0 +1,23 @@
+from calendar import month_name, mdays
+from functools import reduce
+from locale import setlocale, LC_ALL
+
+# Português do Brasil
+setlocale(LC_ALL, 'pt_BR')
+
+
+def mes_com_31(mes):
+    return mdays[mes] == 31
+
+
+def get_nome_mes(mes):
+    return month_name[mes].capitalize()
+
+
+def juntar_meses(todos, nome_mes):
+    return f'{todos}\n- {nome_mes}'
+
+
+print(reduce(juntar_meses,
+             map(get_nome_mes, filter(mes_com_31, range(1, 13))),
+             'Meses com 31 dias:'))
