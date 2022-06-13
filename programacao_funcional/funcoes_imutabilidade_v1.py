a2b7bdc6e3b9f90f3ba77d67b8fdefdfc77c783e
Eduardo Freitas <eduardoflorencio96@gmail.com>
2022-06-13 20:24:34 -0300

    feat: funções imutabilidade #01

---
+++ programacao_funcional/funcoes_imutabilidade_v1.py
@@ -0,0 +1,23 @@
+from functools import reduce
+from operator import add
+
+valores = [30, 10, 25, 70, 100, 94]
+
+print(sorted(valores))
+print(valores)
+
+# função mutável
+# valores.sort()
+# print(valores)
+
+print(min(valores))
+print(max(valores))
+print(sum(valores))
+print(reduce(add, valores))
+print(reversed(valores))
+print(list(reversed(valores)))
+print(valores)
+
+# função mutável
+# valores.reverse()
+# print(valores)
