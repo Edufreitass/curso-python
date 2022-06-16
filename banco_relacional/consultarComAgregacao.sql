7d817342f2c3762d5accaf72648a759ae723a767
Eduardo Freitas <eduardoflorencio96@gmail.com>
2022-06-16 17:33:13 -0300

    feat(MySQL): consultar estados com agregação

---
+++ banco_relacional/consultarComAgregacao.sql
@@ -0,0 +1,17 @@
+SELECT
+    regiao AS 'Região',
+    SUM(populacao) AS Total
+FROM
+    estados
+GROUP BY regiao
+ORDER BY Total DESC;
+
+SELECT
+    SUM(populacao) AS Total
+FROM
+    estados;
+
+SELECT
+    AVG(populacao) AS Total
+FROM
+    estados;
